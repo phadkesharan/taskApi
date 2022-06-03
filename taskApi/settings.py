18dbdf6d0e3b26c3f095641720e50fef76c9d926
Sharan Phadke <phadkesharan@gmail.com>
2022-06-03 20:00:39 +0530

    UPDATE: postgres db connected

--- taskApi/settings.py
+++ taskApi/settings.py
@@ -74,10 +74,23 @@ WSGI_APPLICATION = 'taskApi.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'taskDb',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'Sh@120101',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
+
 }
 
 
