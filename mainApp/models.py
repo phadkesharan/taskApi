d0cd7d5c414f32228f66672e50fe6be8484dbf9c
Sharan Phadke <phadkesharan@gmail.com>
2022-06-03 20:37:26 +0530

    NEW: create database model

--- mainApp/models.py
+++ mainApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class task(models.Model):
+    
+    id = models.UUIDField(
+     primary_key = True,
+     default = uuid.uuid4,
+     editable = False)
+    createdAt = models.DateTimeField(auto_now_add=True)
+
+    name = models.CharField(max_length=200, blank=False, default=None)
+    description = models.CharField(max_length=1000, blank=False, default=None)
+    email = models.EmailField(blank=False, default=None)
+
